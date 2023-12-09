a9adbc11fad42083c364babc0eccdc1779ae6f6a
Hacker00002 <abdulbositsharipovv@gmail.com>
2023-12-09 20:55:35 +0500

    first commit

---
+++ lib/data/header.data.ts
@@ -0,0 +1,22 @@
+[
+  {
+    "header": [
+      {
+        "linkTitle": "About",
+        "linkPath": "/about"
+      },
+      {
+        "linkTitle": "Blog",
+        "linkPath": "/blog"
+      },
+      {
+        "linkTitle": "Gear",
+        "linkPath": "/gear"
+      },
+      {
+        "linkTitle": "Projects",
+        "linkPath": "/project"
+      }
+    ]
+  }
+]
