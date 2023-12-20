6a9a6545adb372725cc5e896ee7838ecbabcc0a2
Hacker00002 <abdulbositsharipovv@gmail.com>
2023-12-20 14:49:03 +0500

    changed some files

--- app/api/subscribers/route.ts
+++ app/api/subscribers/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 // @ts-ignore
 import { db } from '@/lib/db'
 
-export async function GET(res: Response) {
+export async function GET() {
   try {
     const subscribers = await db.subscripbe.findMany()
     return NextResponse.json(subscribers)
