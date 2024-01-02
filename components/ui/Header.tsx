8226ecbfd5910a12197ee0b148557b4f1cf902b6
Hacker00002 <abdulbositsharipovv@gmail.com>
2024-01-02 15:21:18 +0500

    chnaged some files

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -55,7 +55,7 @@ export const Header = () => {
                   href={item.linkPath}
                   className={cn(
                     'text-[#646464] hover:text-primary dark:text-[#b4b4b4] hover:text-zinc-900 dark:hover:text-white transition-all text-sm px-4 py-1.5 rounded-lg',
-                    item.linkPath === pathName && 'transition-all dark:text-white text-zinc-900 bg-[#ffffff1b]',
+                    item.linkPath === pathName && 'transition-all dark:text-white text-zinc-900 bg-[#00000017] dark:bg-[#ffffff1b]',
                   )}
                 >
                   {item.linkTitle}
