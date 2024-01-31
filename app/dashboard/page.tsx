94bef57aa38beb6858cb1e0c56b6d2eea1a1f01e
Hacker00002 <abdulbositsharipovv@gmail.com>
2024-01-31 10:45:17 +0500

    connected my blog with unami cloud with nextjs

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import Script from 'next/script'
-
-export default function Dashboard() {
-  return (
-    <>
-      <Script src="https://us.umami.is/script.js" data-website-id="94cc47c5-56f6-4b2c-b05b-881b076a25de" />
-    </>
-  )
-}
