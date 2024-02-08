7e6d3024366161f91868c1b8787d0cd0cf849fd6
Hacker00002 <abdulbositsharipovv@gmail.com>
2024-02-08 20:54:46 +0500

    test

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -10,12 +10,35 @@ export const metadata: Metadata = {
 
 const ProjectPage = () => {
   return (
-    <div className={'container pb-24'}>
-      <div className={'pt-[9rem] max-md:pt-[8rem] flex flex-col gap-16 md:gap-24'}>
-        <ProjectMain />
-        <ProjectItem />
+    <>
+      <meta charSet="utf-8" />
+      {/*//Facebook Meta Tags */}
+      <meta property="og:url" content="https://a-bd.vercel.app/projects" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Interesting projects with popular frameworks." />
+      <meta property="og:description" content="Here are some of the projects I've worked on." />
+      <meta
+        property="og:image"
+        content="https://opengraph.b-cdn.net/production/documents/0a0926f1-2adc-4cc9-b2ce-82458eea1368.jpg?token=Sth5htvy1OqzIhZfzP5iJ6pPzhbTJEAIMpsqvP5Xj0g&height=640&width=640&expires=33243406322"
+      />
+      {/*// Twitter Meta Tags*/}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="a-bd.vercel.app" />
+      <meta property="twitter:url" content="https://a-bd.vercel.app/projects" />
+      <meta name="twitter:title" content="Interesting projects with popular frameworks." />
+      <meta name="twitter:description" content="Here are some of the projects I've worked on." />
+      <meta name="twitter:image" content="https://a-bd.vercel.appLink preview image URL" />
+      <meta
+        name="twitter:image"
+        content="https://opengraph.b-cdn.net/production/documents/0a0926f1-2adc-4cc9-b2ce-82458eea1368.jpg?token=Sth5htvy1OqzIhZfzP5iJ6pPzhbTJEAIMpsqvP5Xj0g&height=640&width=640&expires=33243406322"
+      />
+      <div className={'container pb-24'}>
+        <div className={'pt-[9rem] max-md:pt-[8rem] flex flex-col gap-16 md:gap-24'}>
+          <ProjectMain />
+          <ProjectItem />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
