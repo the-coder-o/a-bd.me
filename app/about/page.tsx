6b3dad6addbabd6ecea0d72fa52ee77bb7e3fd75
Hacker00002 <abdulbositsharipovv@gmail.com>
2024-02-08 20:40:36 +0500

    added SEO code for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -23,7 +23,7 @@ const AboutPage = () => {
           content="https://opengraph.b-cdn.net/production/documents/0a0926f1-2adc-4cc9-b2ce-82458eea1368.jpg?token=Sth5htvy1OqzIhZfzP5iJ6pPzhbTJEAIMpsqvP5Xj0g&height=640&width=640&expires=33243406322"
         />
         {/*// Twitter Meta Tags*/}
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary" />
         <meta property="twitter:domain" content="a-bd.vercel.app" />
         <meta property="twitter:url" content="https://a-bd.vercel.app/about" />
         <meta name="twitter:title" content="Link preview title" />
