d4faa495fdb7304669d5b61d28c871281eeea21d
Hacker00002 <abdulbositsharipovv@gmail.com>
2024-02-08 20:33:03 +0500

    wrote SEO optimization code for project

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,15 +36,26 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </Script>
       <head>
         <meta charSet="utf-8" />
-        <meta property="twitter:image" content="Twitter link preview image URL" />
-        <meta property="twitter:title" content="Twitter link preview title" />
-        <meta property="twitter:description" content="Twitter link preview description" />
-        <meta property="og:image" content="Link preview image URL" />
+        {/*//Facebook Meta Tags */}
+        <meta property="og:url" content="https://a-bd.vercel.app/" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="Link preview title" />
         <meta property="og:description" content="Link preview description" />
-        <meta property="og:url" content="Canonical link preview URL" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta name="google-site-verification" content="AmY4ge3X_IK5cstwDQKSFQzGH_6vjAAUxArmBV4SduU" />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/documents/0a0926f1-2adc-4cc9-b2ce-82458eea1368.jpg?token=Sth5htvy1OqzIhZfzP5iJ6pPzhbTJEAIMpsqvP5Xj0g&height=640&width=640&expires=33243406322"
+        />
+        {/*// Twitter Meta Tags*/}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="a-bd.vercel.app" />
+        <meta property="twitter:url" content="https://a-bd.vercel.app/" />
+        <meta name="twitter:title" content="Link preview title" />
+        <meta name="twitter:description" content="Link preview description" />
+        <meta name="twitter:image" content="https://a-bd.vercel.appLink preview image URL" />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/documents/0a0926f1-2adc-4cc9-b2ce-82458eea1368.jpg?token=Sth5htvy1OqzIhZfzP5iJ6pPzhbTJEAIMpsqvP5Xj0g&height=640&width=640&expires=33243406322"
+        />
       </head>
       <body className={cn(font.className, 'bg-[#FCFCFC] dark:bg-[#111111]')}>
         <link
