f55d8dd5c834a2545987175af4ddaa21bbb77c32
Hacker00002 <abdulbositsharipovv@gmail.com>
2024-02-10 18:02:33 +0500

    test request"

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
+      disallow: ['/admin', 'privacy'],
     },
     sitemap: 'https://a-bd.me/sitemap.xml',
   }
