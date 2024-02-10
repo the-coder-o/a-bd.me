f8105372279a0602a0b9eed18b293b4179089259
Hacker00002 <abdulbositsharipovv@gmail.com>
2024-02-10 21:08:03 +0500

    changed some files

--- components/ui/header/components/header-mobile.tsx
+++ components/ui/header/components/header-mobile.tsx
@@ -18,12 +18,10 @@ const HeaderMobile = () => {
         <DropdownMenu>
           <DropdownMenuTrigger className={'text-[#646464] dark:text-[#b4b4b4] relative'}>Menu +</DropdownMenuTrigger>
           <DropdownMenuContent className={'absolute left-[-100px] z-[99999999]'}>
-            {HeaderData?.map((links: any) => (
+            {HeaderData?.map((links: any, index) => (
               <DropdownMenuItem
-                className={cn(
-                  'text-[#646464] hover:text-primary dark:text-[#b4b4b4] hover:text-zinc-900 dark:hover:text-white transition-all text-sm px-4 py-1.5 rounded-lg',
-                  links?.linkPath === pathName && 'transition-all dark:text-white text-zinc-900 bg-[#ffffff1b]',
-                )}
+                key={index}
+                className={cn('text-[#646464] hover:text-primary dark:text-[#b4b4b4] hover:text-zinc-900 dark:hover:text-white transition-all text-sm px-4 py-1.5 rounded-lg', links?.linkPath === pathName && 'transition-all dark:text-white text-zinc-900 bg-[#ffffff1b]')}
                 onClick={() => router.push(links?.linkPath)}
               >
                 {links?.linkTitle}
