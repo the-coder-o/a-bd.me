4de381fec90b1c86424ce438219a3f9a03bd8210
Hacker00002 <abdulbositsharipovv@gmail.com>
2024-02-11 21:22:36 +0500

    seo optimized xml urls added some urls

--- app/(home)/components/home-main.tsx
+++ app/(home)/components/home-main.tsx
@@ -8,7 +8,7 @@ const HomeMain = () => {
   return (
     <div>
       <motion.h1 initial={{ y: 20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.1 }} className={'font-bold text-3xl'}>
-        Al-Basit Blog
+        Abdul Basit
       </motion.h1>
       <motion.p initial={{ y: 20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 2 * 0.1 }} className={'dark:text-[#b4b4b4] text-[#646464]'}>
         I write code and make videos.
