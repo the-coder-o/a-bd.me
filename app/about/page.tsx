46fc0d2d9abc98426ffaa8ac4bfdc82be67a4e26
Hacker00002 <abdulbositsharipovv@gmail.com>
2024-02-17 18:06:36 +0500

    optimizing the code and seo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,23 +1,41 @@
+'use client'
+
 import React from 'react'
 
-import { Metadata } from 'next'
+import { NextSeo } from 'next-seo'
 
 import Gallery from '@/components/ui/gallery'
-import About from '@/app/about/component/about'
 import AboutMain from '@/app/about/component/about-main'
+import About from '@/app/about/component/about'
 import MobileDesign from '@/app/about/component/mobile-design'
 
-export const metadata: Metadata = {
-  title: 'About | Abdul Basit',
-  description: 'I am a full-stack software engineer who basically just enjoys creating things.',
-  alternates: {
-    canonical: 'http://www.a-bd.me/about',
-  },
-}
-
-const AboutPage = () => {
+const AboutPage: React.FC = () => {
   return (
     <>
+      <NextSeo
+        title="About | Abdul Basit"
+        description="I am a full-stack software engineer who basically just enjoys creating things."
+        canonical="http://www.a-bd.me/about"
+        openGraph={{
+          url: 'http://www.a-bd.me/about',
+          title: 'Just a quick glimpse.',
+          description: 'I am a full-stack software engineer who basically just enjoys creating things.',
+          images: [
+            {
+              url: 'https://opengraph.b-cdn.net/production/documents/0a0926f1-2adc-4cc9-b2ce-82458eea1368.jpg?token=Sth5htvy1OqzIhZfzP5iJ6pPzhbTJEAIMpsqvP5Xj0g&height=640&width=640&expires=33243406322',
+              width: 640,
+              height: 640,
+              alt: 'Og Image Alt',
+            },
+          ],
+          site_name: 'Abdul Basit Blog',
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+      />
       <meta name="robots" content="index, follow" />
       <meta property="og:url" content="https://a-bd.me/about" />
       <meta name="keywords" content="Abdul Basit Blog's" />
@@ -32,18 +50,18 @@ const AboutPage = () => {
       <meta name="twitter:description" content="I am a full-stack software engineer who basically just enjoys creating things." />
       <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/documents/0a0926f1-2adc-4cc9-b2ce-82458eea1368.jpg?token=Sth5htvy1OqzIhZfzP5iJ6pPzhbTJEAIMpsqvP5Xj0g&height=640&width=640&expires=33243406322" />
       <link rel="canonical" href="https://a-bd.me/about" />
-      <div className={'pt-[9rem] max-md:pt-[8rem] pb-28'}>
-        <div className={'container'}>
+      <main className="pt-[9rem] max-md:pt-[8rem] pb-28">
+        <div className="container">
           <AboutMain />
         </div>
         <MobileDesign />
-        <div className="hidden lg:block mt-[80px]">
+        <section className="hidden lg:block mt-[80px]">
           <Gallery />
-        </div>
-        <div className={'container'}>
+        </section>
+        <div className="container">
           <About />
         </div>
-      </div>
+      </main>
     </>
   )
 }
