503099f16f543b2094b4eaa83d2811455ff68d49
Abdul basit <115723431+the-coder-o@users.noreply.github.com>
2024-02-22 19:20:33 +0500

    added seo optimization for link page

--- components/ui/header/components/header-mobile.tsx
+++ components/ui/header/components/header-mobile.tsx
@@ -21,7 +21,7 @@ const HeaderMobile = () => {
             {HeaderData?.map((links: any, index) => (
               <DropdownMenuItem
                 key={index}
-                className={cn('text-[#646464] hover:text-primary dark:text-[#b4b4b4] !text-[16px] hover:text-zinc-900 dark:hover:text-white transition-all text-sm px-4 py-1.5 rounded-lg', links?.linkPath === pathName && 'transition-all dark:text-white text-zinc-900 bg-[#ffffff1b]')}
+                className={cn('text-[#646464] hover:text-primary dark:text-[#fff] !text-[16px] hover:text-zinc-900 dark:hover:text-white transition-all text-sm px-4 py-1.5 rounded-lg', links?.linkPath === pathName && 'transition-all dark:text-white text-zinc-900 bg-[#ffffff1b]')}
                 onClick={() => router.push(links?.linkPath)}
               >
                 {links?.linkTitle}
